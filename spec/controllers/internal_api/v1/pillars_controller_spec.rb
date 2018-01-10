7f69a893afa078b9ab40687d27313c492a0dd6f7
Rafael Fernández López <ereslibre@ereslibre.es>
2018-01-10 12:39:02 +0100

    Add `CertificateService` proxy model that will tie `Certificate` to any service

--- spec/controllers/internal_api/v1/pillars_controller_spec.rb
+++ spec/controllers/internal_api/v1/pillars_controller_spec.rb
@@ -6,15 +6,39 @@ RSpec.describe InternalApi::V1::PillarsController, type: :controller do
 
   render_views
 
+  let(:certificate) { Certificate.create certificate: "certificate" }
+  let(:expected_response) do
+    {
+      dashboard:  "dashboard.example.com",
+      registries: [
+        {
+          url:         "https://example.com",
+          mirror:      nil,
+          certificate: "certificate"
+        }
+      ]
+    }
+  end
+
   before do
     http_login
     request.accept = "application/json"
   end
 
   describe "GET /pillar" do
-    it "retrieves the pillar contents" do
+    before do
+      Pillar.create pillar: "dashboard", value: "dashboard.example.com"
+      DockerRegistry.create url: "https://example.com", certificate: certificate
+    end
+
+    it "has the expected response status" do
       get :show
       expect(response.status).to eq 200
+    end
+
+    it "has the expected contents" do
+      get :show
+      expect(json).to match expected_response
     end
   end
 
